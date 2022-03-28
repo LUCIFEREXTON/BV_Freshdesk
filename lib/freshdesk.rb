3fa4bb253e22c1676da248137925f0415d4fcb1e
LUCIFEREXTON <gupta13utkarsh@gmail.com>
2022-03-28 12:04:00 +0530

    Initial commit

---
+++ lib/freshdesk.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "freshdesk/version"
+
+module Freshdesk
+	class Engine < Rails::Engine; end
+  class Error < StandardError; end
+  # Your code goes here...
+end
